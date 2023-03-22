8b0c51f1dabbd9e4dfb3c14de9a196a1b0791c88
irmoad91 <i.moad33@gmail.com>
2023-03-21 17:20:10 -0700

    void print_alphabet(void)

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - print alphabet in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; i <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
