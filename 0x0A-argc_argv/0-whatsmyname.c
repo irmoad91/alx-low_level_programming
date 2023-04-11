051d529812532c1d8e24b7dd8fa92fa2e7437165
irmoad91 <i.moad33@gmail.com>
2023-04-10 18:17:06 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,8 +6,9 @@
  * @argv: array of argument
  * Return: return to 0
  */
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	printf("%s\n", *argv);
+	(void)argc;
+	printf("%s\n", argv[0]);
 	return (0);
 }
