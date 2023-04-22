3f8dba1ddb776d5ab0b89eaee3f7df75972a75f1
irmoad91 <i.moad33@gmail.com>
2023-04-21 18:39:38 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1 = 0, num2 = 0;
+	int num1 = 0, num2 = 0 result = 0;
 	char op;
 
 	if (argc != 4)
@@ -33,6 +33,7 @@ int main(int argc, char *argv[])
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	printf("%d\n", get_op_func(argv[2])(num1, num2));
+	result = (get_op_func(argv[2]))(num1, num2)
+	printf("%d\n", result);
 	return (0);
 }
