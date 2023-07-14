fb7ec9e49a29d6df07b27f132ebe28e99417170a
irmoad91 <i.moad33@gmail.com>
2023-07-14 16:32:55 -0700

    a function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,13 +21,12 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	flrd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (flrd == -1)
-		return (-1);
 	wr = write(flrd, text_content, len);
+
+	if (flrd == -1 || wr == -1)
+		return (-1);
+
 	close(flrd);
 
-	if (wr == -1)
-		return (-1);
-
-	return (-1);
+	return (1);
 }
