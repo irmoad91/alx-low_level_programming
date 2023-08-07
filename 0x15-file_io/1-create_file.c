6a1c9ca3f319796d62d048fc8576f673682d3039
irmoad91 <i.moad33@gmail.com>
2023-08-07 15:44:02 -0700

    a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - a function that creates a file
+ * @filename: the file to create
+ * @text_content: the string to write the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int flrd;
+	int wr;
+	int len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+	{
+		len = strlen(text_content);
+	}
+
+	flrd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	wr = write(flrd, text_content, len);
+
+	if (flrd == -1 || wr == -1)
+		return (-1);
+
+	close(flrd);
+
+	return (1);
+}
